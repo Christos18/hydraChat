daf3aad003f50246c39605612399a2252b804e32
Christos Demetriades <ChristosUK18@gmail.com>
2014-01-15 03:19:04 +0000

    Added logic to determine if browser is webRTC capable

--- webrtc.js
+++ webrtc.js
@@ -11,3 +11,40 @@ var stun_server = "stun.l.google.com:19302";
 
 /***********************************************/
 
+if (navigator.getUserMedia)
+{
+	rtc_peer_connection = RTCPeerConnection;
+    rtc_session_description = RTCSessionDescription;
+	get_user_media = navigator.getUserMedia;
+	connect_stream_to_src = function (media_stream, media_element)
+	{
+		media_element.srcObject = media_stream;
+		media_element.play();
+	};
+}
+else if (navigator.mozGetUserMedia)
+{
+	rtc_peer_connection = mozRTCPeerConnection;
+    rtc_session_description = mozRTCSessionDescription;
+	get_user_media = navigator.mozGetUserMedia;
+	connect_stream_to_src = function (media_stream, media_element)
+	{
+		media_element.mozSrcObject = media_stream;
+		media_element.play();
+	};
+}
+else if (navigator.webkitGetUserMedia)
+{
+	rtc_peer_connection = webkitRTCPeerConnection;
+    rtc_session_description = webkitRTCSessionDescription;
+	get_user_media = navigator.webkitGetUserMedia;
+	connect_stream_to_src = function (media_stream, media_element)
+	{
+		media_element.src = webkitURL.createObjectURL(media_stream);
+	};
+}
+else
+{
+	alert("Your browser does not support WebRTC. Please use Chrome of Firefox.")
+	webrtc_capable = false;
+}
